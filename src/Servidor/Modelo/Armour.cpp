04b2c10a36aa06e9e2b5ddf33ddb5d8c3b4893f4
alasinofranco3 <falasino@fi.uba.ar>
2020-06-19 23:40:13 -0300

    agrego armadura

---
+++ src/Servidor/Modelo/Armour.cpp
@@ -0,0 +1,38 @@
+//
+// Created by franco on 19/6/20.
+//
+
+#include "Armour.h"
+
+Armour::Armour() : chestMinDef(0), chestMaxDef(0), shieldMinDef(0), shieldMaxDef(0),
+    helmetMinDef(0), helmetMaxDef(0) {
+
+}
+
+Armour::Armour(int chestMinDef, int chestMaxDef, int shieldMinDef, int shieldMaxDef
+    , int helmetMinDef, int helmetMaxDef) : chestMinDef(chestMinDef), chestMaxDef(chestMaxDef),
+    shieldMinDef(shieldMinDef), shieldMaxDef(shieldMaxDef), helmetMinDef(helmetMinDef),
+    helmetMaxDef(helmetMaxDef){
+
+}
+
+void Armour::equipChest(int minDef, int maxDef) {
+    chestMinDef = minDef;
+    chestMaxDef = maxDef;
+}
+void Armour::equipShield(int minDef, int maxDef) {
+    shieldMinDef = minDef;
+    shieldMaxDef = maxDef;
+}
+void Armour::equipHelmet(int minDef, int maxDef) {
+    helmetMinDef = minDef;
+    helmetMaxDef = maxDef;
+}
+
+int Armour::use(int damage, Calculator& calculator) {
+    int defense = calculator.calculateDefense(chestMinDef, chestMaxDef
+            , shieldMinDef, shieldMaxDef, helmetMinDef, helmetMaxDef);
+    int result = damage - defense;
+    if (result < 0) return 0;
+    return result;
+}
---
+++ src/Servidor/Modelo/Armour.h
@@ -0,0 +1,30 @@
+//
+// Created by franco on 19/6/20.
+//
+
+#ifndef ARGENTUM_ARMOUR_H
+#define ARGENTUM_ARMOUR_H
+
+#include "Calculator.h"
+
+//enum ArmourType {chest, shield, helmet};
+
+class Armour {
+    int chestMinDef, chestMaxDef;
+    int shieldMinDef, shieldMaxDef;
+    int helmetMinDef, helmetMaxDef;
+public:
+    Armour();
+    Armour(int chestMinDef, int chestMaxDef, int shieldMinDef, int shieldMaxDef
+    , int helmetMinDef, int helmetMaxDef);
+    void equipChest(int minDef, int maxDef);
+    void equipShield(int minDef, int maxDef);
+    void equipHelmet(int minDef, int maxDef);
+    //Devuelve el danio final que recibira el personaje luego de defenderse
+    //OTRA OPCION SERIA QUE DEVUELVA SOLAMENTE EL VALOR DE DEFENSA Y HACER LA RESTA EN OTRO LADO
+    int use(int damage, Calculator& calculator);
+    ~Armour() = default;
+};
+
+
+#endif //ARGENTUM_ARMOUR_H
