b19be20492fd6b81ef21c71cf377cc16f3900f75
Agustin Leguizamon <agustinmleguizamon@gmail.com>
2020-07-13 17:47:21 -0300

    Solucioname esto franco, <3

--- src/Cliente/src/main.cpp
+++ src/Cliente/src/main.cpp
@@ -1,11 +1,39 @@
 #include "client_qt_loginmediator.h"
+//#include "../../Proxy/src/common_proxy_socket.h"
+//#include "../../Proxy/src/common_message.h"
+#include <thread>
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+//void server(ProxySocket& proxySocket);
+
+int QtApp(int argc, char*argv[]);
+
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     LoginMediator loginMediator;
     loginMediator.show();
 
+
+    //ProxySocket proxySocket;
+    //std::thread proxyServer(server, proxySocket);
+  //  QtApp(argc, argv);
     return a.exec();
+
+//    return 0;
 }
+
+/*
+void server(ProxySocket& proxySocket){
+    std::cout << "server running" << std::endl;
+    int i = 0;
+    while(i < 10){
+        std::unique_ptr<Message> msg = proxySocket.readServer();
+        std::cout << msg->getId() << std::endl;
+        i++;
+    }
+}
+*/
+
+int QtApp(int argc, char*argv[]){
+    return 0;
+ }
