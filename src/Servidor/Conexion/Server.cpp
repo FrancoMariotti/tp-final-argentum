4f09cd3b13c65f1842cc1d191facfc526a092cdc
FrancoMariotti <fmariotti@fi.uba.ar>
2020-07-19 17:51:38 -0300

    refactor loop servidor

--- src/Servidor/Conexion/Server.cpp
+++ src/Servidor/Conexion/Server.cpp
@@ -19,100 +19,81 @@ void Server::readInput() {
     keepTalking = false;
 }
 
+void Server::sendInitialMessages(MessageSerializer& serializer,std::string& username,int connectionId) {
+        connectionsTable[username] = connectionId;
+        std::queue<Message*> initialMessages = game.initializeWorld();
+        while(!initialMessages.empty()) {
+            Message* update = initialMessages.front();
+            std::string updateData = serializer.serialize(update);
+            if(update->getId() == SPAWN_PC_MESSAGE_ID) {
+                clients.broadcast(SPAWN_PC_MESSAGE_ID,updateData);
+            } else {
+                clients.sendMessage(connectionId,update->getId(),updateData);
+            }
+            initialMessages.pop();
+            delete update;
+        }
+}
+
+
+void Server::handleEvent(MessageSerializer& serializer,std::unique_ptr<Message>& msg) {
+    std::cout << "MESSAGE ID:" << msg->getId() <<std::endl;
+    if (msg->getId() == LOGIN_MESSAGE_ID) {
+        t_client_login data = msg->getLoginData();
+        bool result = game.login(data.username, data.password);
+        int answer = result ? 1:0;
+        clients.sendMessage(msg->getConnectionlId(),answer);
+        if (result) {
+            sendInitialMessages(serializer,data.username,msg->getConnectionlId());
+        }
+    } else if (msg->getId() == SIGNUP_MESSAGE_ID) {
+        t_client_login data = msg->getLoginData();
+        bool result = game.signup(data.username, data.password);
+        int answer = result ? 1:0;
+        clients.sendMessage(msg->getConnectionlId(),answer);
+    } else if (msg->getId() == CONNECT_MESSAGE_ID) {
+        t_create_connect data = msg->getConnectData();
+        game.createPlayer(data.username,data.race,data.charClass);
+        sendInitialMessages(serializer,data.username,msg->getConnectionlId());
+    } else if (msg->getId() == DISCONNECT_MESSAGE_ID) {
+        std::queue<Message*> pcSpawnsUpdate = game.disconnectPlayer(msg->getUsername());
+        Message* update = pcSpawnsUpdate.front();
+        std::string updateData = serializer.serialize(update);
+        clients.broadcast(SPAWN_PC_MESSAGE_ID,updateData);
+    } else if (msg->getId() == MOVEMENT_MESSAGE_ID) {
+        location_t location = msg->getLocation();
+        Offset offset(location.x, location.y);
+        game.movePlayer(location.id,offset);
+    } else if (msg->getId() == COMMAND_MESSAGE_ID) {
+        game.executeCommand(msg);
+    } else if (msg->getId() == USE_ITEM_MESSAGE_ID) {
+        int connectionId = msg->getConnectionlId();
+        auto result = std::find_if(
+                connectionsTable.begin(),
+                connectionsTable.end(),
+                [connectionId](std::pair<std::string,int> connection)
+                { return connection.second == connectionId; });
+        game.equip(result->first, msg->getIndex());
+    } else if (msg->getId() == PLAYER_ATTACK_MESSAGE_ID) {
+        location_t attackInfo = msg->getLocation();
+        Position pos(attackInfo.x, attackInfo.y);
+        game.attack(attackInfo.id, pos);
+    }
+}
+
 void Server::start() {
     std::cout << "Server is running" << std::endl;
 
     std::thread first(&Server::readInput,this);
     MessageSerializer serializer;
     this->clientAcceptor.start();
-    //falta agregar un try/catch en caso de que salte alguna excepcion finalizar el server.
+
     while(keepTalking) {
         std::chrono::time_point<std::chrono::system_clock> start, end;
         start = std::chrono::system_clock::now();
         std::list<std::unique_ptr<Message>> messages = events.consume();
         for (auto & msg : messages) {
-            std::cout << "MESSAGE ID:" << msg->getId() <<std::endl;
-            if (msg->getId() == LOGIN_MESSAGE_ID) {
-                t_client_login data = msg->getLoginData();
-                bool result = game.login(data.username, data.password);
-                int answer = result ? 1:0;
-                clients.sendMessage(msg->getConnectionlId(),answer);
-                if (result) {
-                    connectionsTable[data.username] = msg->getConnectionlId();
-                    std::queue<Message*> initialMessages = game.initializeWorld();
-                    while(!initialMessages.empty()) {
-                        Message* update = initialMessages.front();
-                        std::string updateData = serializer.serialize(update);
-                        if(update->getId() == SPAWN_PC_MESSAGE_ID) {
-                            clients.broadcast(SPAWN_PC_MESSAGE_ID,updateData);
-                        } else {
-                            clients.sendMessage(msg->getConnectionlId(),update->getId(),updateData);
-                        }
-                        initialMessages.pop();
-                        delete update;
-                    }
-                }
-            }
-            if (msg->getId() == SIGNUP_MESSAGE_ID) {
-                t_client_login data = msg->getLoginData();
-                bool result = game.signup(data.username, data.password);
-                int answer = result ? 1:0;
-                clients.sendMessage(msg->getConnectionlId(),answer);
-            }
-            if (msg->getId() == CONNECT_MESSAGE_ID) {
-                t_create_connect data = msg->getConnectData();
-                game.createPlayer(data.username,data.race,data.charClass);
-                connectionsTable[data.username] = msg->getConnectionlId();
-                std::queue<Message*> initialMessages = game.initializeWorld();
-                while(!initialMessages.empty()) {
-                    Message* update = initialMessages.front();
-                    std::string updateData = serializer.serialize(update);
-                    if(update->getId() == SPAWN_PC_MESSAGE_ID) {
-                        clients.broadcast(SPAWN_PC_MESSAGE_ID,updateData);
-                    } else {
-                        clients.sendMessage(msg->getConnectionlId(),update->getId(),updateData);
-                    }
-                    initialMessages.pop();
-                    delete update;
-                }
-            }
-
-            if (msg->getId() == DISCONNECT_MESSAGE_ID) {
-                //HAY QUE HACER EL BROADCAST PARA DECRILE A TODOS LOS JUGADORES
-                //QUE DEBEN DE DEJAR DE RENDERIZAR AL JUGADOR DESCONECTADO
-                //Esto es una idea que me surgio ,usar el mismo metodo que usamos para inicializar los personajes
-                //nose que tan bien esta pero quizas funcione para que todos borren al jugador que se desconecto
-                std::queue<Message*> pcSpawnsUpdate = game.disconnectPlayer(msg->getUsername());
-                Message* update = pcSpawnsUpdate.front();
-                //CREO QUE EL MENSAJE SE PODRIA SERIALIZAR PORQUE ESTOY USANDO EL MENSAJE SPAWNPC QUE YA EXISTE
-                std::string updateData = serializer.serialize(update);
-                //CREO QUE ESTE MENSAJE POR LO QUE VI QUE HACE EN EL CLIENTE SERVIRIAP PARA QUE BORRE AL JUGADOR
-                //DESCONECTADO,YA QUE EN EL UPDATEPLAYABLERENDERABLES, HACE UN CLEAR Y DESPUES AGREGA TODOS
-                //PLAYERS QUE LE LLEGAN
-                clients.broadcast(SPAWN_PC_MESSAGE_ID,updateData);
-            }
-            if (msg->getId() == MOVEMENT_MESSAGE_ID) {
-                location_t location = msg->getLocation();
-                Offset offset(location.x, location.y);
-                game.movePlayer(location.id,offset);
-            }
-            if (msg->getId() == COMMAND_MESSAGE_ID) {
-                game.executeCommand(msg);
-            }
-            if (msg->getId() == USE_ITEM_MESSAGE_ID) {
-                int connectionId = msg->getConnectionlId();
-                auto result = std::find_if(
-                        connectionsTable.begin(),
-                        connectionsTable.end(),
-                        [connectionId](std::pair<std::string,int> connection)
-                                    { return connection.second == connectionId; });
-                game.equip(result->first, msg->getIndex());
-            }
-            if (msg->getId() == PLAYER_ATTACK_MESSAGE_ID) {
-                location_t attackInfo = msg->getLocation();
-                Position pos(attackInfo.x, attackInfo.y);
-                game.attack(attackInfo.id, pos);
-            }
+            handleEvent(serializer,msg);
         }
         float looptimeInSeconds = 60 * 0.001;
         game.updateModel(looptimeInSeconds);
--- src/Servidor/Conexion/Server.h
+++ src/Servidor/Conexion/Server.h
@@ -12,10 +12,14 @@ class Server {
     std::map<std::string,int> connectionsTable;
     ProtectedConnections clients;
     ProtectedList<std::unique_ptr<Message>> events;
+
+    void sendInitialMessages(MessageSerializer &serializer, std::string &username, int connectionId);
+    void handleEvent(MessageSerializer &serializer, std::unique_ptr<Message>& msg);
 public:
     void readInput();
     explicit Server(const std::string& service,const std::string& configFile,
             const std::string& playersInfoMapFile, const std::string& playersInfoFile);
+
     void start();
 };
 
